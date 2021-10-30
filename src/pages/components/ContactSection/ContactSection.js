9df10f9995ed2ed6bd7221c2c413071e7474e6b4
srhimel <himusr@gmail.com>
2021-10-31 03:57:09 +0600

    email js done

--- src/pages/components/ContactSection/ContactSection.js
+++ src/pages/components/ContactSection/ContactSection.js
@@ -1,10 +1,22 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Form, Row, Button } from 'react-bootstrap';
 import useMap from '../../../hooks/useMap';
+import emailjs from 'emailjs-com';
+
 
 const ContactSection = () => {
+    const [mailNoti, setMailNoti] = useState('');
     const map = useMap(23.916566, 90.218745);
+    const handleEmailSend = e => {
+        e.preventDefault();
+        emailjs.sendForm('service_y04nrih', 'template_0udyjmd', e.target, 'user_NjE14Xy3XM7XVtLOVvSgE')
+            .then(res => {
+                setMailNoti("email send successfull");
+                e.target.reset();
+            });
+    }
+
     return (
         <section>
             <Container>
@@ -19,20 +31,21 @@ const ContactSection = () => {
                         {map}
                     </Col>
                     <Col md={6}>
-                        <Form className="border p-3">
+                        <Form className="border p-3" onSubmit={handleEmailSend}>
                             <Form.Group className="mb-3">
                                 <Form.Label>Your Name</Form.Label>
-                                <Form.Control type="text" placeholder="Your Name" />
+                                <Form.Control type="text" placeholder="Your Name" name="name" />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Email Address" />
+                                <Form.Control type="email" placeholder="Email Address" name="email" />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Your Message</Form.Label>
-                                <Form.Control as="textarea" rows={3} placeholder="Your Message" />
+                                <Form.Control as="textarea" rows={3} placeholder="Your Message" name="message" />
                             </Form.Group>
-                            <Button variant="info">Send Message</Button>
+                            <p>{mailNoti.length > 0 && "Email sent"}</p>
+                            <Button variant="info" type="submit">Send Message</Button>
                         </Form>
                     </Col>
 
